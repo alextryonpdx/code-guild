e144ce935febdbffaa8a2aad2461fc5ac5cf8f4f
student <alextryonpdx@gmail.com>
2015-01-09 16:17:17 -0800

    fixed some display issues. deck resets after each game. user is prompted before returning to main menu after game is dealt. would be useful (and easy) to add user prompting between dealt rounds (player 1 player 2 and river), could deal new games.

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,17 +1,24 @@
-deck = {
-1:["",'cA','c2','c3','c4','c5','c6','c7','c8','c9','c10','cJ','cQ','cK'],
-2:["",'sA','s2','s3','s4','s5','s6','s7','s8','s9','s10','sJ','sQ','sK'],
-3:["",'dA','d2','d3','d4','d5','d6','d7','d8','d9','d10','dJ','dQ','dK'],
-4:["",'hA','h2','h3','h4','h5','h6','h7','h8','h9','h10','hJ','hQ','hK']
-}
+
 game = '0'
 while str(game) != '9':
-
+	hand1 = []
+	hand2 = []
+	flip = []
+	flop = []
+	river = []
+	deck = {
+	1:["",'cA','c2','c3','c4','c5','c6','c7','c8','c9','c10','cJ','cQ','cK'],
+	2:["",'sA','s2','s3','s4','s5','s6','s7','s8','s9','s10','sJ','sQ','sK'],
+	3:["",'dA','d2','d3','d4','d5','d6','d7','d8','d9','d10','dJ','dQ','dK'],
+	4:["",'hA','h2','h3','h4','h5','h6','h7','h8','h9','h10','hJ','hQ','hK']
+	}
 	print "Let's Play Cards"
 	print "1. Five Card Stud"
 	print "2. Seven Card Stud"
 	print "3. Texas Hold'em"
 	print "9. Quit"
+
+
 	game = raw_input("What do you want to play?")
 
 	if str(game) == '1':
@@ -19,32 +26,39 @@ while str(game) != '9':
 		from random import randint
 
 		a = 0
-		print "player one"
-		while a != 11: 
+		print "\n player one"
+		while a != 12: 
 			if a < 5:
 				suit = randint(1,4)
 				card = randint(1,13)
 				while deck[suit][card] == 'x':
 					suit = randint(1,4)
 					card = randint(1,13)
-				print deck[suit][card]
+				# print deck[suit][card]
+				hand1.append(deck[suit][card])
 				deck[suit][card] = 'x'
 				a += 1
 			if a == 5:
+				print hand1
 				print "\n player two"
 				a += 1
-			if a < 11 and a > 5:
+			if a < 12 and a > 5:
 				suit = randint(1,4)
 				card = randint(1,13)
 				while deck[suit][card] == 'x':
 					suit = randint(1,4)
 					card = randint(1,13)
-				print deck[suit][card]
+				# print deck[suit][card]
+				hand2.append(deck[suit][card])
 				deck[suit][card] = 'x'
 				a += 1
+			if a == 11:
+				print hand2
+				print '\n'
 		end_game = raw_input("Return to menu? y, n: ")
 		if end_game.lower == 'y':
 			a += 1
+		
 		else:
 			game = '0'
 		
@@ -54,17 +68,19 @@ while str(game) != '9':
 
 		a = 0
 		print "player one"
-		while a != 15: 
+		while a != 16: 
 			if a < 7:
 				suit = randint(1,4)
 				card = randint(1,13)
 				while deck[suit][card] == 'x':
 					suit = randint(1,4)
 					card = randint(1,13)
-				print deck[suit][card]
+				# print deck[suit][card]
+				hand1.append(deck[suit][card])
 				deck[suit][card] = 'x'
 				a += 1
 			if a == 7:
+				print hand1
 				print "\n player two"
 				a += 1
 			if a < 15 and a > 7:
@@ -73,8 +89,13 @@ while str(game) != '9':
 				while deck[suit][card] == 'x':
 					suit = randint(1,4)
 					card = randint(1,13)
-				print deck[suit][card]
+				# print deck[suit][card]
+				hand2.append(deck[suit][card])
 				deck[suit][card] = 'x'
+				a += 1
+			if a == 15:
+				print hand2
+				print "\n"
 				a += 1
 		end_game = raw_input("Return to menu? y, n: ")
 		if end_game.lower == 'y':
@@ -87,17 +108,19 @@ while str(game) != '9':
 		from random import randint
 		a = 0
 		print "player one"
-		while a != 22: 
+		while a != 23: 
 			if a < 6:
 				suit = randint(1,4)
 				card = randint(1,13)
 				while deck[suit][card] == 'x':
 					suit = randint(1,4)
 					card = randint(1,13)
-				print deck[suit][card]
+				# print deck[suit][card]
+				hand1.append(deck[suit][card])
 				deck[suit][card] = 'x'
 				a += 1
 			if a == 6:
+				print hand1
 				print "\n player two"
 				a += 1
 			if a < 12 and a > 6:
@@ -106,10 +129,12 @@ while str(game) != '9':
 				while deck[suit][card] == 'x':
 					suit = randint(1,4)
 					card = randint(1,13)
-				print deck[suit][card]
+				# print deck[suit][card]
+				hand2.append(deck[suit][card])
 				deck[suit][card] = 'x'
 				a += 1
 			if a == 12:
+				print hand2
 				print "\n the flop"
 				a += 1
 			if a == 13:
@@ -126,10 +151,12 @@ while str(game) != '9':
 				while deck[suit][card] == 'x':
 					suit = randint(1,4)
 					card = randint(1,13)
-				print deck[suit][card]
+				# print deck[suit][card]
+				flop.append(deck[suit][card])
 				deck[suit][card] = 'x'
 				a += 1
 			if a == 16:
+				print flop
 				print "\n the flip"
 				a += 1
 			if a == 17:
@@ -146,10 +173,12 @@ while str(game) != '9':
 				while deck[suit][card] == 'x':
 					suit = randint(1,4)
 					card = randint(1,13)
-				print deck[suit][card]
+				# print deck[suit][card]
+				flip.append(deck[suit][card])
 				deck[suit][card] = 'x'
 				a += 1
 			if a == 19:
+				print flip
 				print "\n the river"
 				a += 1
 			if a == 20:
@@ -166,8 +195,12 @@ while str(game) != '9':
 				while deck[suit][card] == 'x':
 					suit = randint(1,4)
 					card = randint(1,13)
-				print deck[suit][card]
+				# print deck[suit][card]
+				river.append(deck[suit][card])
 				deck[suit][card] = 'x'
+				a += 1
+			if a == 22:
+				print river
 				a += 1
 		end_game = raw_input("Return to menu? y, n: ")
 		if end_game.lower == 'y':
