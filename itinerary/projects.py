3299ec117a6e4774f782ab89efcd2d3ddb6f1a66
student <alextryonpdx@gmail.com>
2015-01-26 14:46:30 -0800

    got a menu. beginings of contactlist class but having trouble gettting it started just like other class. still need as much practice with class __init__ definitions.

--- itinerary/projects.py
+++ itinerary/projects.py
@@ -30,6 +30,7 @@ class Project(object):
 
 # adds new list items by appending self.args list
     def add_item(self):
+        self.__str__()
         new_item = raw_input("Enter item to add: ")
         self.args.append(new_item)
 
@@ -47,16 +48,55 @@ class Project(object):
         edited_item = raw_input("Enter edited item: ")
         self.args[to_edit - 1] = edited_item
 
-
+# menu function for single project list
+    def project_menu(self):
+        print "what would you like to do with this project?"
+        print "1. add an item"
+        print "2. edit an item"
+        print "3. remove an item"
+        print "4. view your list"
+        project_menu_choice = int(raw_input("... "))
+        if project_menu_choice == 1:
+            self.add_item()
+        if project_menu_choice == 2:
+            self.edit_item()
+        if project_menu_choice == 3:
+            self.delete_item()
+        if project_menu_choice == 4:
+            self.__str__()
+            menu = raw_input("press enter to continue")
+            self.project_menu()
+        else:
+            print "you made an improper selection"
+            self.project_menu()
 
 
 """
+class ProjectList(object):
+
+    def __init__(self, *args):
+        self.args = {}
+        for item in args:
+            self.args[item] = item
+
+    def __str__(self):
+        for item in self.args:
+            print item
+
+
+
 test_project = Project("test", 'one', "two", "three")
-test_project.__str__()
-test_project.add_item()
-test_project.__str__()
-test_project.delete_item()
-test_project.__str__()
-test_project.edit_item()
-test_project.__str__()
+test_list = Project("list", "first", "second", "third")
+
+test_ProjectList = ProjectList(test_project, test_list)
+test_ProjectList.__str__()
+#test_project.__str__()
+#test_project.add_item()
+#test_project.__str__()
+#test_project.delete_item()
+#test_project.__str__()
+#test_project.edit_item()
+#test_project.__str__()
+#test_project.project_menu()
+
 """
