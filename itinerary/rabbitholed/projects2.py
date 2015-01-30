cfc0570d0ab8737d45dd6a691a401a4895d07c18
student <alextryonpdx@gmail.com>
2015-01-29 16:48:30 -0800

    having problems recognizing task type attributes in display after creating new list. messed with new list creating process and waterfalled our problems.

--- itinerary/rabbitholed/projects2.py
+++ itinerary/rabbitholed/projects2.py
@@ -1,5 +1,6 @@
 class Task(object):
     def __init__(self, task, due_date=None, complete=False, *contacts):
+        self.task_list = [task]
         self.task = task
         self.due_date = due_date
         self.contacts = []
@@ -40,17 +41,33 @@ class Projects(object):
 
     # shows detailed view of single project
     def view_project(self, project_name):
-        for item in self.projects[project_name]:
-            item.__str__()
+        print self.projects.keys()
+        x = 1
+        for item in self.projects[project_name].task_list:
+            print "%s. %s" % (x, item)
+            x += 1
+            if item.complete == True:
+                print "    *COMPLETE*"
+                # print self.task
+            else:
+                # print self.task
+                if item.due_date != None and item.due_date != False:
+                    print "    Due: %s" % item.due_date
+                if item.contacts != None:
+                    for name in item.contacts:
+                        print "    > %s" % name
+                    # print "     %s" % name.contact_name
+                else:
+                    pass
 
     # creats a project key but no task
-    def create_project(self, project_name):
-        self.projects[project_name] = []
+    def create_project(self, project_name, task_one):
+        self.projects[project_name] = Task(task_one)
 
     # creates task and adds it to the end of selected project
     def create_task(self, project_name, task):
-        new_task = Task(task)
-        self.projects[project_name].append(new_task)
+        #new_task = Task(task)
+        self.projects[project_name].task_list.append(task)
 
     # remove task indexed by project and by index
     def remove_task(self, project_name, task_del):
@@ -72,19 +89,89 @@ class Projects(object):
     def task_remove_contact(self, project_name, task, remove_contact):
         self.projects[project_name][task].contacts.pop(remove_contact)
 
+# returns project name
+def get_project():
+    user.view_projects()
+    get = raw_input("Select a project: ")
+    return get
 
+# returns project name, task index (int)
+def get_task():
+    x = get_project()
+    user.view_project(x)
+    select_task = int(raw_input("select task by number"))
+    select_task -= 1
+    # print select_task, x
+    return x, select_task
+
+
+def new_text():
+    text = raw_input("enter something awesome: ")
+    return text
+
+def projects_menu():
+    while True:
+        print "PROJECTS MENU"
+        print "1. new project"
+        print "2. view projects"
+        print "3. select a project"
+        print "4. return to user menu"
+        cmd = int(raw_input("make a selection: "))
+        if cmd == 1:
+            x = new_text()
+            newt = raw_input("enter task number one: ")
+            user.create_project(x, newt)
+            single_project_menu(x)
+        if cmd == 2:
+            user.view_projects()
+        if cmd == 3:
+            single_project_menu(get_project())
+        if cmd == 4:
+            # user menu
+            pass
+        else:
+            print "try harder"
+
+def single_project_menu(y):
+    user.view_project(y)
+    print "what would you like to do with this project?"
+    print "1. add a new task"
+    print "2. edit a task"
+    print "3. remove an project"
+    print "4. view your project"
+    print "5. return to projects menu"
+    cmd = int(raw_input("make a selection: "))
+    if cmd == 1:
+        new_task = raw_input("enter new task: ")
+        create_task(y, new_task)
+    if cmd == 2:
+        # get task
+        # edit task menu
+        pass
+    if cmd == 3:
+        user.view_project(y)
+        rem_task = (int(raw_input("select task by number: ")) - 1)
+        remove_task(y, rem_task)
+    if cmd == 4:
+        user.view_project(y)
+    if cmd == 5:
+        projects_menu()
+    else:
+        print "try harder"
 
 #username.projects["projectname"].add_project()
 
-test = Projects()
-test.create_project("school")
+user = Projects()
+user.create_project("school", "test")
 
-test.create_task("school", "homework")
-test.create_task("school", "quiz")
-for item in test.projects["school"]:
-    item.__str__()
-test.remove_task("school", 1)
-for item in test.projects["school"]:
-    item.__str__()
-test.view_projects()
-# test.add_project("school", new_task)
+user.create_task("school", "homework")
+user.create_task("school", "quiz")
+#for item in user.projects["school"]:
+ #   item.__str__()
+#user.remove_task("school", 1)
+#for item in user.projects["school"]:
+ #   item.__str__()
+#user.complete_task("school", 0)
+#user.view_project("school")
+projects_menu()
+# user.add_project("school", new_task)
